58fd12f67dafa651b23f9fcfd58354ce1556f87d
Marcelo Caldas <mcq1@cdc.gov>
2017-08-18 17:23:28 -0400

    adding ability to run postman on build

---
+++ node_modules/newman/src/runners/AbstractRunner.js
@@ -0,0 +1,21 @@
+var jsface = require("jsface");
+
+/**
+ * @class AbstractRunner
+ * @param CollectionJson {JSON} Takes a JSON as the input
+ */
+var AbstractRunner = jsface.Class({
+    constructor: function (collection) {
+        this.collection = collection || [];
+    },
+
+    /**
+     * Executes the runner
+     * All runners override this function
+     */
+    execute: function () {
+        return this;
+    }
+});
+
+module.exports = AbstractRunner;
